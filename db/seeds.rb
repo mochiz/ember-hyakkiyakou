b551addd3ff61bae54b650712e0ddc850461bbda
mochiz <mochi.hiro43@gmail.com>
2014-07-30 15:52:40 +0900

    妖怪モデルを作成した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+creatures = YAML.load_file(Rails.root.join('db', 'fixtures', 'hyakkiyakou.yml'))
+creatures.each do |key, value|
+  creature = Creature.find_or_create_by(name: value['name'], kana: value['kana'])
+  creature.update(wiki_url: value['wiki_url'],
+                  avatar: value['avatar'],
+                  description: value['description'],
+                  notes: value['notes'])
+end
